1793ae021a67b75debcf711d34e4d23ed4fce868
Vadim Medvedev <vadim130809@gmail.com>
2024-03-09 20:59:21 +0300

    Revert "get weather data from api"
    This reverts commit aba7925639562ecbb9d432414ee9f4ac5e78baa6.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
-import {FC, useEffect, useState} from 'react'
-import WeatherService from './API/WeatherService'
-import { useFetching } from './hooks/useFetching'
+import {FC} from 'react'
 
 export const App: FC = () => {
-  const [weather, setWeather] = useState<WeatherResponse>()
-  const [fetchWeather, weatherLoading, weatherLoadingError] = useFetching(async () => {
-    const {lat, lon}: TownResponse = await WeatherService
-    .getLocation('Moscow')
-   const coordinates: Coordinates = {
-     lat, lon
-   }
-   const weatherData:WeatherResponse = await WeatherService
-     .getAll(coordinates)
-   return setWeather(weatherData)
-  })
-
-  useEffect(() => {
-    fetchWeather()
-  }, [])
-  
   return (
     <>
-      {weatherLoading && <h1>Loading...</h1>}
-      {weatherLoadingError && <h1>Error {weatherLoadingError}</h1>}
-      {!weatherLoading && !weatherLoadingError
-        && String(weather?.cod)}
+      WORK!
     </>
   )
 }
