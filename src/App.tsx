581b0db7e7193d43c35dfc624e40258183f9ef20
Vadim Medvedev <vadim130809@gmail.com>
2024-03-11 22:04:04 +0300

    add useFetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import {FC, useState} from 'react'
+import {FC, useEffect, useState} from 'react'
 import { Header } from './components/Header'
 import { LocationContext } from './context'
+import WeatherService from './API/WeatherService'
+import useFetching from './hooks/useFetching'
 
 export const App: FC = () => {
   const [location, setLocation] = useState<string>('')
-  
+  const [fetchLocation, isLocationLoading, locationLoadingError] = useFetching(async () => {
+    const {lat, lon}: TownResponse = await WeatherService.getLocation(location)
+    return { lat, lon }
+  })
+
+  useEffect(() => {
+    fetchLocation()
+  }, [location])
+
   return (
     <>
       <LocationContext.Provider value={{
